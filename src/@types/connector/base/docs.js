cfeb74dcf180b47a21104e911b34e249364b7b00
James Pearce <github@tripleodeon.com>
2025-09-20 15:09:13 +1300

    [base] Remove

--- src/@types/connector/base/docs.js
+++
@@ -1,7 +0,0 @@
-/**
- * The connector/base module.
- * @packageDocumentation
- * @module connector/base
- * @since v0.0.0
- */
-/// connector/base
--- src/connector/base/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './table.ts';
-export * from './tables.ts';
-export * from './value.ts';
-export * from './values.ts';
