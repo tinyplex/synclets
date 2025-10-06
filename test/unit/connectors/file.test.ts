b2e2dd7748768d1dc17567946923ca1dfa2fe82b
James Pearce <github@tripleodeon.com>
2025-10-07 12:15:03 +1300

    [tests] File over ws

--- test/unit/connectors/file.test.ts
+++ test/unit/connectors/file.test.ts
@@ -6,8 +6,11 @@ import {
   createFileDataConnector,
   createFileMetaConnector,
 } from 'synclets/connector/fs';
+import {createWsServer} from 'synclets/server/ws';
 import {createMemoryTransport} from 'synclets/transport/memory';
+import {createWsTransport} from 'synclets/transport/ws';
 import {getUniqueId} from 'synclets/utils';
+import {WebSocketServer} from 'ws';
 import {describeConnectorTests} from '../common.ts';
 
 test('file', async () => {
@@ -38,4 +41,23 @@ describeConnectorTests(
   (depth: number, {tempDir}) =>
     createFileMetaConnector(depth, join(tempDir, getUniqueId())),
   (uniqueId: string) => createMemoryTransport({poolId: uniqueId}),
+);
+
+describeConnectorTests(
+  'file over ws',
+  async () => ({
+    tempDir: await mkdtemp(tmpdir() + sep),
+    wsServer: createWsServer(new WebSocketServer({port: 9000})),
+  }),
+  async ({tempDir, wsServer}) => {
+    await rm(tempDir, {recursive: true, force: true});
+    wsServer.destroy();
+  },
+  (depth: number, {tempDir}) =>
+    createFileDataConnector(depth, join(tempDir, getUniqueId())),
+  (depth: number, {tempDir}) =>
+    createFileMetaConnector(depth, join(tempDir, getUniqueId())),
+  (uniqueId: string) =>
+    createWsTransport(new WebSocket('ws://localhost:9000/' + uniqueId)),
+  5,
 );
