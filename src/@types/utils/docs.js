8e1a16b6e827ae21f6e57a7225ae076de8542a87
James Pearce <github@tripleodeon.com>
2025-11-08 15:50:29 +1300

    [docs] Utils

--- src/@types/utils/docs.js
+++ src/@types/utils/docs.js
@@ -5,3 +5,64 @@
  * @since v0.0.0
  */
 /// utils
+
+/**
+ * The jsonString function converts an unknown value to a normalized JSON
+ * string.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// jsonString
+
+/**
+ * The jsonParse function parses a JSON string that may have come from
+ * jsonString.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// jsonParse
+
+/**
+ * The getUniqueId function generates a unique ID string of the requested
+ * length.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// getUniqueId
+
+/**
+ * The getHash function produces a Hash for a given string payload.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// getHash
+
+/**
+ * The isTimestamp function is a type guard that identifies Timestamp values.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// isTimestamp
+
+/**
+ * The isAtom function is a type guard that identifies Atom values.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// isAtom
+
+/**
+ * The getPartsFromPacket function splits a packet into the destination and body
+ * parts.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// getPartsFromPacket
+
+/**
+ * The getPacketFromParts function encodes a destination and body into a packet
+ * string.
+ * @category Utils
+ * @since v0.0.0
+ */
+/// getPacketFromParts
--- src/@types/utils/index.d.ts
+++ src/@types/utils/index.d.ts
@@ -2,18 +2,26 @@
 
 import {Atom, Hash, Timestamp} from '@synclets/@types';
 
+/// jsonString
 export function jsonString(obj: unknown): string;
 
+/// jsonParse
 export function jsonParse(str: string): any;
 
+/// getUniqueId
 export function getUniqueId(length?: number): string;
 
+/// getHash
 export function getHash(string: string): Hash;
 
+/// isTimestamp
 export function isTimestamp(thing: unknown): thing is Timestamp;
 
+/// isAtom
 export function isAtom(thing: unknown): thing is Atom;
 
+/// getPartsFromPacket
 export function getPartsFromPacket(packet: string): [to: string, body: string];
 
+/// getPacketFromParts
 export function getPacketFromParts(to: string, body: string): string;
